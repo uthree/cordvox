ef84961898ce2859c77ab7770846190959a7236f
uthree <33905956+uthree@users.noreply.github.com>
2024-06-01 10:40:42 +0900

    fix

--- module/cordvox/__init__.py
+++ module/cordvox/__init__.py
@@ -32,9 +32,11 @@ class Cordvox(L.LightningModule):
         loss_stft = multiscale_stft_loss(wf, fake)
 
         if self.discriminator_active:
-            logits_fake, _ = D(fake)
+            logits_fake, feats_fake = D(fake)
+            logits_real, feats_real = D(wf)
             loss_adv = generator_adversarial_loss(logits_fake)
-            loss_G = loss_stft + loss_adv * 2.0
+            loss_feat = feature_matching_loss(feats_real, feats_fake)
+            loss_G = loss_stft + loss_feat + loss_adv * 2.0
         else:
             loss_G = loss_stft
 
@@ -64,6 +66,7 @@ class Cordvox(L.LightningModule):
             loss_dict = {
                 "MS-STFT": loss_stft.item(),
                 "Generator Adversarial": loss_adv.item(),
+                "Feature Matching": loss_feat.item(),
                 "Discriminator Adversarial": loss_D.item(),
             }
         else:
