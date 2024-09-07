91472cdb48bcd578cc15621f9f1fc84ee304fb7c
uthree <uthree271828@gmail.com>
2024-09-07 14:25:13 +0900

    fix

--- module/cordvox/generator.py
+++ module/cordvox/generator.py
@@ -50,14 +50,54 @@ class ResBlock2(nn.Module):
             xt = c1(xt)
             x = x + xt
         return x
+    
+
+class HarmonicOscillator(nn.Module):
+    def __init__(
+            self,
+            sample_rate,
+            frame_size,
+            num_harmonics=8,
+            noise_scale=0.03
+        ):
+        super().__init__()
+        self.sample_rate = sample_rate
+        self.frame_size = frame_size
+        self.num_harmonics = num_harmonics
+        self.noise_scale = noise_scale
+
+        self.weights = nn.Parameter(torch.ones(1, num_harmonics, 1))
+
+    def forward(self, f0, uv):
+        '''
+        Args:
+            f0: fundamental frequency shape=[N, 1, L]
+            uv: unvoiced=0 / voiced=1 flag, shape=[N, 1, L]
+        Output
+            shape=[N, 1, L * frame_size]
+        '''
+        with torch.no_grad():
+            f0 = F.interpolate(f0, scale_factor=self.frame_size, mode='linear')
+            voiced_mask = F.interpolate(uv, scale_factor=self.frame_size)
+            mul = (torch.arange(self.num_harmonics, device=f0.device) + 1).unsqueeze(0).unsqueeze(2)
+            fs = f0 * mul
+            integrated = torch.cumsum(fs / self.sample_rate, dim=2)
+            phi = torch.rand(1, self.num_harmonics, 1, device=f0.device)
+            rad = 2 * math.pi * ((integrated + phi) % 1)
+            noise = torch.randn_like(rad)
+            harmonics = torch.sin(rad) * voiced_mask + noise * self.noise_scale
+            voiced_part = harmonics + noise * self.noise_scale
+            unvoiced_part = noise * 0.333
+            source = voiced_part * voiced_mask + unvoiced_part * (1 - voiced_mask)
+            source = (source * F.normalize(self.weights, p=2.0, dim=1)).sum(dim=1, keepdim=True)
+        return source
 
 
-class Generator(nn.Module):
+class PeriodicHiFiGANFilter(nn.Module):
     def __init__(
             self,
             n_mels=80,
             sample_rate=24000,
-            num_harmonics=32,
             upsample_initial_channels=512,
             resblock_type="1",
             resblock_kernel_sizes=[3, 7, 11],
@@ -70,7 +110,6 @@ class Generator(nn.Module):
         self.num_kernels = len(resblock_kernel_sizes)
         self.num_upsamples = len(upsample_rates)
         self.sample_rate = sample_rate
-        self.num_harmonics = num_harmonics
         self.frame_size = 1
         
         for u in upsample_rates:
@@ -92,7 +131,7 @@ class Generator(nn.Module):
             p = (k-u)//2
             downs.append(weight_norm(nn.Conv1d(c2, c1, k, u, p)))
             self.ups.append(weight_norm(nn.ConvTranspose1d(c1, c2, k, u, p)))
-        downs.append(weight_norm(nn.Conv1d(num_harmonics, c2, 7, 1, 3)))
+        downs.append(weight_norm(nn.Conv1d(1, c2, 7, 1, 3)))
         self.downs = nn.ModuleList(list(reversed(downs)))
         
         self.resblocks = nn.ModuleList()
@@ -105,17 +144,9 @@ class Generator(nn.Module):
         self.apply(init_weights)
 
 
-    def forward(self, x, f0):
-        # oscillator
-        uv = (f0 > 20.0).to(f0.dtype)
-        f0 = F.interpolate(f0, scale_factor=self.frame_size, mode='linear')
-        uv = (F.interpolate(uv, scale_factor=self.frame_size, mode='linear') > 0.99).to(f0.dtype)
-        mul = (torch.arange(self.num_harmonics, device=f0.device) + 1).unsqueeze(0).unsqueeze(2)
-        integrated = torch.cumsum(f0 / self.sample_rate, dim=2) * mul
-        rad = 2 * torch.pi * (integrated % 1)
-        s = torch.sin(rad) * uv
-
+    def forward(self, x, source):
         # downsamples
+        s = source
         source_signals = []
         for i in range(len(self.downs)):
             s = self.downs[i](s)
@@ -141,3 +172,30 @@ class Generator(nn.Module):
         x = self.conv_post(x)
         x = torch.tanh(x)
         return x
+    
+
+class Generator(nn.Module):
+    def __init__(
+            self,
+            source_module,
+            filter_module,
+            source_type="harmonic",
+            filter_type="period_hifigan",
+            min_frequency=20.0
+    ):
+        super().__init__()
+        self.min_frequency=min_frequency
+
+        if source_type == "harmonic":
+            self.source_module = HarmonicOscillator(**source_module)
+        else:
+            raise "invailed source module type"
+        
+        if filter_type == "period_hifigan":
+            self.filter_module = PeriodicHiFiGANFilter(**filter_module)
+
+    def forward(self, x, f0):
+        uv = (f0 > self.min_frequency).to(f0.dtype)
+        source = self.source_module(f0, uv)
+        output = self.filter_module(x, source)
+        return output
